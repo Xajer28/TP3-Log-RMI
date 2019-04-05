f3141f5a22c8c2272920d76c39401026bf3da97d
tgattaz <thomas.gattaz@gmail.com>
2019-04-05 09:28:19 +0200

    package et correctif sur GestionnaireAnneau

---
+++ src/tp/GestionnaireAnneau/Donnees.java
@@ -0,0 +1,19 @@
+package tp.GestionnaireAnneau;
+
+import java.io.Serializable;
+
+public class Donnees implements Serializable{
+
+  private static final long serialVersionUID = -6199402168457446943L;
+
+  public int idsucesseur;
+  public String adrIpsucesseur;
+  public boolean lance_election;
+
+  public Donnees(int idsucesseur, String adrIpsucesseur, boolean lance_election){
+    this.idsucesseur=idsucesseur;
+    this.adrIpsucesseur=adrIpsucesseur;
+    this.lance_election=lance_election;
+  }
+
+}
